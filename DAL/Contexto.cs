50c831dcaff87a12534f15193c22bb5a0faae63b
Marcos Daniel Rosario Infante <marcos_rosario@ucne.edu.do>
2024-06-03 18:32:40 -0400

    ahora si tengo el contexto.

--- DAL/Contexto.cs
+++ DAL/Contexto.cs
@@ -1,6 +1,11 @@
-﻿namespace AP1_P1_MarcosRosario.DAL
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace AP1_P1_MarcosRosario.DAL
 {
-    public class Contexto
-    {
-    }
+	public class Contexto : DbContext
+	{
+		public Contexto(DbContextOptions<Contexto> options) 
+			: base(options) { }
+
+	}
 }
