2686f74c243cf7f337160992925fadb5c6b64ee4
Marcos Daniel Rosario Infante <marcos_rosario@ucne.edu.do>
2024-06-03 21:14:28 -0400

    modificacion del contexto

--- DAL/Contexto.cs
+++ DAL/Contexto.cs
@@ -1,11 +1,12 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using AP1_P1_MarcosRosario.Models;
+using Microsoft.EntityFrameworkCore;
 
-namespace AP1_P1_MarcosRosario.DAL
+namespace AP1_P1_MarcosRosario.DAL;
+
+public class Contexto : DbContext
 {
-	public class Contexto : DbContext
-	{
-		public Contexto(DbContextOptions<Contexto> options) 
-			: base(options) { }
-
-	}
+	public Contexto(DbContextOptions<Contexto> options)
+		: base(options) { }
+	public DbSet<Articulos> Articulos { get; set; }
 }
+
